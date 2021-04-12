25c71b5470eb6208359f21652f33777182000dbb
Andriy Parkhomenko <Andriy.Parkhomenko@grenoble-inp.org>
2021-04-12 17:55:19 +0200

    Supression du doGet inutile et déplacement des attributs au-dessus

--- src/main/java/controleur/Publication.java
+++ src/main/java/controleur/Publication.java
@@ -29,6 +29,9 @@ import modele.Histoire;
 public class Publication extends HttpServlet {
 
     
+    @Resource(name = "jdbc/projetWeb")
+    private DataSource ds;
+    
       private void erreurBD(HttpServletRequest request,
             HttpServletResponse response, DAOException e)
         throws ServletException, IOException {
@@ -47,21 +50,6 @@ public class Publication extends HttpServlet {
               histoireDAO.publierHistoire(histoire);
           }      
       }
-
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -83,9 +71,6 @@ public class Publication extends HttpServlet {
         
         response.sendRedirect("accueil");
     }
-    
-    @Resource(name = "jdbc/projetWeb")
-    private DataSource ds;
     
 
 }
